7b4241774bf290d64292eaa70facddcc502c9967
Omar Monib <omar.monib.03@gmail.com>
2026-06-29 17:04:51 +0300

    test auth

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,23 +5,8 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-function buildClient(): PrismaClient {
-  if (process.env.TURSO_DATABASE_URL) {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const { createClient } = require("@libsql/client");
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const { PrismaLibSQL } = require("@prisma/adapter-libsql");
-    const libsql = createClient({
-      url: process.env.TURSO_DATABASE_URL,
-      authToken: process.env.TURSO_AUTH_TOKEN ?? "",
-    });
-    const adapter = new PrismaLibSQL(libsql);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return new PrismaClient({ adapter } as any);
-  }
-  return new PrismaClient();
+export const prisma = global.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
 }
-
-export const prisma = global.prisma ?? buildClient();
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
